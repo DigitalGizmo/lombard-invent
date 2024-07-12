56ff43dfd7b99585a873c3afcf32ad8cb577430d
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-07-12 17:18:33 -0400

    to kaput with choices

--- src/App.svelte
+++ src/App.svelte
@@ -2,66 +2,57 @@
   import { tweened } from 'svelte/motion';
   import { cubicOut } from 'svelte/easing';
 
+  import challenges from './lib/challenges.json';
   import clouds from './assets/images/sky.png'
   import landscape from './assets/images/landscape.png'
   import lumber from './assets/images/lumber.png'
   import horses from './assets/images/horses.png'
-  import Counter from './lib/Counter.svelte'
+  import power_oxen from './assets/images/option-oxen.png'
+  import power_steam from './assets/images/option-steam-tractor.png'
+  import power_trolly from './assets/images/option-trolley.png'
+  import power_kaput from './assets/images/horses-kaput.png'
 
   let frameHeight = 900;
 
-  // const size = tweened(1, {
-  //   duration: 300,
-  //   easing: cubicOut
-  // });
-  // function handleClick() {
-  //   $size += 1;
-  // }
-
-  const haulerX = tweened(200, {
-    duration: 2000,
+  const haulerX = tweened(100, {
+    duration: 3000,
     easing: cubicOut
   });
   
   let popQuestion = false;
+
+  let challengeIndex = -1;
+  // let question = challenges[0].question;
   
   let stillHauling = false;
   async function handleHauler() {
   // function handleHauler() {
     stillHauling = true;
-
     // $haulerX += 300;
-    // await $haulerX += 300;
-    // await haulerX.set(500);
-    // await haulerX.set(500);
-    await haulerX.update((haulerX) => haulerX + 300)
-
-
+    await haulerX.update((haulerX) => haulerX + 200)
     stillHauling = false;
-    // callback when finished?
+    challengeIndex +=1;
   }
 
-  $: if ($haulerX === 600) {popQuestion = true}
+  // $: if ($haulerX === 600) {popQuestion = true}
 
 </script>
 
 <main>
   <div>
     <h1>Be the Inventor</h1>
-    <!-- <Counter /> -->
 
-    <!-- <button on:click={handleClick} 
-    style="transform: scale({$size}); transform-origin: 0 0">
-      embiggen
-    </button> -->
-    
     <button on:click={handleHauler}>
       start
     </button>
-    stillHauling: {stillHauling}
-    {#if popQuestion}
-    <p>Horses don't cut it, what would be better?</p>
-    {/if}
+    <p>
+      <!-- stillHauling: {stillHauling} -->
+      {#if challengeIndex > -1}
+        {challenges[challengeIndex].question}
+      {:else}
+        --
+      {/if}
+    </p>
   </div>
 
   <div> <!-- like image-panel -->
@@ -80,11 +71,21 @@
           <g transform="translate({$haulerX} 400)">
             <image  href="{lumber}"/>
             <!-- width="200px" -->
-            <image href="{horses}"
-              transform="translate(220 -120)"
-            />
-            
+            <g  transform="translate(220 -120)">
+              {#if challengeIndex === -1}
+                <image href="{horses}"/>
+              {:else if challengeIndex === 0}
+                <image href="{power_kaput}"/>
+              {/if}
+            </g>
           </g>
+          {#if challengeIndex === 0}
+            <g transform="translate(300 600)">
+              <image href="{power_oxen}" transform="translate(300 0)"/>
+              <image href="{power_steam}" transform="translate(550 0)"/>
+              <image href="{power_trolly}" transform="translate(800 0)" width="250"/>
+            </g>
+          {/if}
         </svg>
       </div> <!-- end image panel image -->
     </div> <!-- /image-panel-fixed -->
