f7a6dac3a1f1f3fdd171084a5f1804ad781dbe03
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-08-12 17:40:29 -0400

    options present on mount - consistent landing

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
   const assetPath = "https://dev.digitalgizmo.com/msm-ed/lombard-invent-assets/"
   // const assetPath = ""
   let challengeIndex = 0;
+  let isFeedback = false;
   const movementDuration = 2000; // 5000
   // let vwidth = 1200;
   // let vheight = 800;
@@ -15,45 +16,73 @@
   let choiceObjectBounds = {top: 100, left:20, bottom:50, right:50};
   let positionA = { x: 0, y: 0};
 
+  let optionAElement;
+
   let haulerLeft = 600;
   let haulerTop = 400;
-  let haulerWidth = 200;
-  let haulerHeight = 150;
+  let haulerWidth = 300;
+  let haulerHeight = 250;
   // let option1Left = 300;
-  // let optionY = 200;
+  let optionTop = 500;
   // let option1LandingX = 20;
   let option1LandingXOffset = 18;
   let optionLandingYOffset = -100;
 
   onMount(() => {
-    // vwidth = Math.max(document.documentElement.clientWidth || 
-    //   0, window.innerWidth || 0);
-    // vheight = Math.max(document.documentElement.clientHeight || 
-    //   0, window.innerHeight || 0);
-    // get the hauler (the horse part) location in x and y
+
+
     let hauler = document.getElementById('hauler');
     let rect = hauler.getBoundingClientRect();
-    haulerLeft = rect.left;
-    haulerTop = rect.top;
-    haulerWidth = rect.right - rect.left;
-    haulerHeight = rect.top - rect.bottom;
+    haulerLeft = Math.round(rect.left);
+    haulerTop = Math.round(rect.top);
+    haulerWidth =  Math.round(rect.right) - haulerLeft;
+    haulerHeight = Math.round(rect.bottom) - haulerTop;
+    console.log('haulerLeft:' + haulerLeft + ' haulerTop:' + haulerTop + 
+    ' haulerWidth:' + haulerWidth + ' haulerHeight:' + haulerHeight)
+
+    let optionARect = optionAElement.getBoundingClientRect();
+    // option1Left = optionARect.left;
+    optionTop = optionARect.top;
+    console.log('optionTop: ' + optionTop);
+    option1LandingXOffset = haulerLeft - optionARect.left;  
+    optionLandingYOffset = -(Math.round(optionARect.top) - haulerTop);  
+    console.log('distance from opt top to haul top: ' + optionLandingYOffset)
+
   })
 
-  async function calcOptionOffsets() {
-    await tick();
-    console.log('finished waiting')
-    // option1
-    let option1 = document.getElementById('option1');
-    let option1Rect = option1.getBoundingClientRect();
-    // option1Left = option1Rect.left;
-    // optionY = option1Rect.top;
-    option1LandingXOffset = haulerLeft - option1Rect.left;  
-    optionLandingYOffset = haulerTop - option1Rect.top;  
-  }
+  // async function calcHaulerOffsets() {
+  //   await tick();
+  //   console.log('finished waiting')
+
+  //   let hauler = document.getElementById('hauler');
+  //   let rect = hauler.getBoundingClientRect();
+  //   haulerLeft = Math.round(rect.left);
+  //   haulerTop = Math.round(rect.top);
+  //   haulerWidth =  Math.round(rect.right) - haulerLeft;
+  //   haulerHeight = Math.round(rect.bottom) - haulerTop;
+  //   console.log('haulerLeft:' + haulerLeft + ' haulerTop:' + haulerTop + 
+  //   ' haulerWidth:' + haulerWidth + ' haulerHeight:' + haulerHeight)
+  //   calcOptionOffsets();
+
+  // }
+
+  // async function calcOptionOffsets() {
+  //   await tick();
+  //   // option1
+  //   // let optionA = document.getElementById('optionA');
+  //   // let optionARect = optionA.getBoundingClientRect();
+  //   let optionARect = optionAElement.getBoundingClientRect();
+  //   // option1Left = optionARect.left;
+  //   optionTop = optionARect.top;
+  //   console.log('optionTop: ' + optionTop);
+  //   option1LandingXOffset = haulerLeft - optionARect.left;  
+  //   optionLandingYOffset = -(Math.round(optionARect.top) - haulerTop);  
+  //   console.log('distance from opt top to haul top: ' + optionLandingYOffset)
+  // }
 
   $: if (challengeIndex === 1) {
     console.log('just turned 1')
-    calcOptionOffsets();
+    // calcHaulerOffsets();
   }
 
   const cloudsX = tweened(0, {
@@ -77,17 +106,21 @@
 
   function dragStop(e) {
     /* Landing position of a given option is relative to its start point
-    * not relative to the viewport */
+    * not relative to the viewport
+    * All Y offset numbers are "upside down": negative numbers fro option start */
     console.log('stoped at x: ' + e.detail.offsetX + ' y: ' +
       e.detail.offsetY)
     if (e.detail.offsetX > option1LandingXOffset && 
       e.detail.offsetX < (option1LandingXOffset + haulerWidth - 40) &&
-      e.detail.offsetY > optionLandingYOffset && 
-      e.detail.offsetY < (optionLandingYOffset - haulerHeight - 30)
+      e.detail.offsetY > optionLandingYOffset && // -400 is gt -500
+      e.detail.offsetY < (optionLandingYOffset + haulerHeight - 30 ) 
+      // -400 is lt (-500 + 200) aka -400 is lt -300
     ) {
       // stick on target
-      positionA = {x: option1LandingXOffset + 50, 
-        y: optionLandingYOffset + 40} 
+      positionA = {x: option1LandingXOffset, 
+        y: optionLandingYOffset} 
+      // determine feedback
+      isFeedback = true;
     } else { // back to home base
       positionA = {x: 0, y: 0} // defaultBx
     }
@@ -117,29 +150,39 @@
   </header>
 
   <div class="hauler">
-    <img src="{assetPath}images/lumber.png" alt="lumber" />
+    <img src="{assetPath}images/lumber.png" alt="lumber"/>
     <img src="{assetPath}images/horses.png" alt="horses" id="hauler"/>
   </div>
 
   <article>
     <h2>{challenges[challengeIndex].title}</h2>
-    <p>{challenges[challengeIndex].question} </p>
+    {#if isFeedback}
+      <!-- <p>{challenges[challengeIndex].options[0].feedback} </p> -->
+      <p>well, oxen are too slow, and they don't like the cold </p>
+    {:else}
+      <p>{challenges[challengeIndex].question} </p>
+      {#if challengeIndex === 0}
+        <button on:click={nextMove}>
+          start
+        </button>
+      {/if}   
+    {/if}
+    <p>
+      -- vwidth:  haulerLeft: {haulerLeft} 
+      haulerHeight: {haulerHeight} 
+      option1LandingXOffset: {option1LandingXOffset}
+      optionLandingYOffset: {optionLandingYOffset}
+    </p>
 
-    {#if challengeIndex === 0}
-      <button on:click={nextMove}>
-        start
-      </button>
-    {/if}    
-    -- vwidth:  haulerLeft: {haulerLeft} 
-    haulerHeight: {haulerHeight} 
-    option1LandingXOffset: {option1LandingXOffset}
   </article>
 
-  {#if challengeIndex === 1}
-    <div class="options">
-      <div class="option1">
+  <div class="options">
+    <div class="option1" >
+      
+      <!-- {#if challengeIndex === 1} -->
         <img src="{assetPath}images/option-oxen.png" 
-          id="option1" alt="option 1: oxen" 
+          id="optionA" alt="option 1: oxen" 
+          bind:this={optionAElement}
           use:draggable={{ 
             position: positionA,
             bounds: choiceObjectBounds,
@@ -150,6 +193,12 @@
           on:neodrag:end={dragStop}      
         />
         <h3>Oxen</h3>
+      <!-- {:else}
+          <img src="{assetPath}images/option-blank.png" 
+          alt="option 1: oxen"/>
+      {/if} -->
+
+
       </div>
       <div class="option2">
         <img src="{assetPath}images/option-steam-tractor.png" alt="option 2: steam" />
@@ -160,6 +209,5 @@
         <h3>Electric Trolly</h3>
       </div>
     </div>
-  {/if}
   
 </div><!--/wrapper-->
