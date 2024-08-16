59f89901ac526fc056978fd4e662acac1cbf03f1
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-08-16 13:13:33 -0400

    option parameter successfully passed

--- src/App.svelte
+++ src/App.svelte
@@ -12,14 +12,13 @@
   let isFeedback = false; // otherwise question/challenge
   let correctnessState = 0;
   let optionToHide = "";
-  const movementDuration = 5000; // 5000
+  const movementDuration = 2000; // 5000
   // let vwidth = 1200;
   // let vheight = 800;
   let textVisible = true;
   let optionsVisible = false;
 
   let choiceObjectBounds = {top: 100, left:20, bottom:50, right:50};
-  let positionA = { x: 0, y: 0};
   let optionPositions = [
     { x: 0, y: 0}, { x: 0, y: 0}, { x: 0, y: 0}, { x: 0, y: 0}
   ]
@@ -75,11 +74,6 @@
     }, 10);
   }  
 
-  // $: if (challengeIndex === 1) {
-  //   console.log('just turned 1')
-  //   calcOptionOffsets();
-  // }
-
   const cloudsX = tweened(0, {
     duration: movementDuration,
     easing: cubicOut
@@ -93,13 +87,10 @@
   async function nextMove() {
     console.log('before timeout')
     textVisible = false;
-
     landX.update((landX) => landX - 15);
     await cloudsX.update((cloudsX) => cloudsX - 8);
-
     setTimeout(() => {
       console.log('after timeout')
-      // correctnessState = 1;
       // chalengIndex change will change main "horse" image
       challengeIndex +=1;
       // isFeedback = true;
@@ -129,7 +120,7 @@
       console.log('display Options')
       optionsVisible = true;
       calcOptionOffsets();
-    }, 3000);
+    }, 2000);
   }
 
   // Short move for wring
@@ -142,29 +133,25 @@
     displayFeedback();
   }
 
-  function dragStop(e) {
+  function dragStop(e, _chosenOptionIndex) { //  _chosenOptionIndex
     /* Landing position of a given option is relative to its start point
     * not relative to the viewport
     * All Y offset numbers are "upside down": negative numbers fro option start */
-    console.log('stoped at x: ' + e.detail.offsetX + ' y: ' +
-      e.detail.offsetY)
+    // console.log('stoped at x: ' + e.detail.offsetX + ' y: ' +
+    //   e.detail.offsetY)
+    console.log('opt index: ' + _chosenOptionIndex);
     if (e.detail.offsetX > option1LandingXOffset && 
       e.detail.offsetX < (option1LandingXOffset + haulerWidth - 40) &&
       e.detail.offsetY > optionLandingYOffset && // -400 is gt -500
       e.detail.offsetY < (optionLandingYOffset + haulerHeight - 30 ) 
       // -400 is lt (-500 + 200) aka -400 is lt -300
     ) {
-      // // determine feedback
-      // isFeedback = true;
-      // // index change will change main "horse" image
+      // index change will change main "horse" image
       // No longer stick option on target
-      // positionA = {x: option1LandingXOffset, 
-      //   y: optionLandingYOffset} 
       // Hide option instead
       optionToHide = "hide-option";
       chosenOptionIndex = 1;
       // Put it away
-      // positionA = {x: 0, y: 0}
       optionPositions[1] = {x: 0, y: 0};
       // optionPositions[1].y = 0;
       // Trigger short movement
@@ -173,10 +160,7 @@
       shortMove();
       // Short move will trigger feedback and options
     } else { // back to home base
-      // positionA = {x: 0, y: 0} // defaultBx
       optionPositions[1] = {x: 0, y: 0};
-      // optionPositions[1].x = 0;
-      // optionPositions[1].y = 0;
     }
   }
 </script>
@@ -225,11 +209,11 @@
         {/if}   
       {/if}
     {/if}
-    <!-- <p>Debug: challengeIndex: {challengeIndex}, 
+    <p>Debug: challengeIndex: {challengeIndex}, 
       chosenOptionIndex: {chosenOptionIndex},
       correctnessState: {correctnessState},
       imageName: {challenges[challengeIndex].options[chosenOptionIndex].correctnessStates[correctnessState].imageName}
-    </p> -->
+    </p>
 
   </article>
 
@@ -244,22 +228,24 @@
         <img src="{assetPath}images/{challenges[challengeIndex].options[1].correctnessStates[correctnessState].imageName}.png" 
           class="{optionToHide}"
           id="optionA" alt="option 1: oxen" 
+          
           bind:this={optionAElement}
           use:draggable={{ 
-            // position: positionA,
             position: optionPositions[1],
             bounds: choiceObjectBounds,
             onDrag: ({offsetX, offsetY}) => {
-              // positionA = {x: offsetX, y: offsetY};
               optionPositions[1] = {x: offsetX, y: offsetY};
             }
           }}  
-          on:neodrag:end={dragStop}      
-        />
+          on:neodrag:end={(e) => dragStop(e, 1)}     
+          />
+          <!-- on:neodrag:end={dragStop} -->
         <h3>Oxen</h3>
       </div>
       <div class="option2">
+
         <img src="{assetPath}images/tractor.png" alt="option 2: steam" />
+
         <h3>Steam Tractor</h3>
       </div>
       <div class="option3">
