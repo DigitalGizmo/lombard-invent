abd51068bfb78167da9c0269c0dc4275ed78b1a1
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-10-04 17:33:45 -0400

    delete unused files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import './app.css'
 import App from './App.svelte'
-import AppSVG from './AppSVG.svelte'
 // import AppModel from './AppModel.svelte'
-// import TestImg from './TestImg.svelte'
 
 const app = new App({
-// const app = new AppSVG({
   target: document.getElementById('app'),
 })
 
